3d0e9e226d82fee342b91c52e5ba29f95596e957
minkyoung kim <minry20415@naver.com>
2022-10-25 14:54:13 +0900

    [update] 10.25 / spring-practice 예외처리, userNull() 적용->error 확인

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,6 +1,7 @@
 package dao;
 
 import domain.User;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.sql.*;
 
@@ -11,7 +12,7 @@ public class UserDao {
         this.connectionMaker = connectionMaker;
     }
 
-    public void add(User user) throws ClassNotFoundException {
+    public void add(User user) {
         try {
             Connection c = connectionMaker.makeConnection();
 
@@ -29,7 +30,7 @@ public class UserDao {
         }
     }
 
-    public User getById(String id) throws ClassNotFoundException {
+    public User getById(String id){
 
         try {
             Connection c = connectionMaker.makeConnection();
@@ -38,10 +39,17 @@ public class UserDao {
             ps.setString(1, id);
             ResultSet rs = ps.executeQuery();
             rs.next();
-            User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+            User user = null;
+            if(rs.next()) {
+                user = new User(rs.getString("id"),
+                                rs.getString("name"),
+                                rs.getString("password"));
+            }
             rs.close();
             ps.close();
             c.close();
+
+            if(user == null) throw new EmptyResultDataAccessException(1);
 
             return user;
 
@@ -50,31 +58,70 @@ public class UserDao {
         }
     }
 
-    public void deleteAll() throws SQLException, ClassNotFoundException {
-        Connection c = connectionMaker.makeConnection();
-        PreparedStatement ps = c.prepareStatement("delete from users");
-        ps.executeUpdate();
-        ps.close();
-        c.close();
+    public void deleteAll() throws SQLException{
+        Connection c = null;
+        PreparedStatement ps = null;
+        try {
+            c = connectionMaker.makeConnection();
+            ps = c.prepareStatement("delete from users");
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        }finally {
+            if(ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
+            if(c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                }
+            }
+        }
     }
 
-    public int getCount() throws SQLException, ClassNotFoundException {
-        Connection c = connectionMaker.makeConnection();
-        PreparedStatement ps = c.prepareStatement("select count(*) from users");
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        int cnt = rs.getInt(1);
-        rs.close();
-        ps.close();
-        c.close();
-        return cnt;
+    public int getCount() throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            c = connectionMaker.makeConnection();
+            ps = c.prepareStatement("select count(*) from users");
+            rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            throw e;
+        }finally {
+            if(rs != null){
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                }
+            }
+            if(ps != null){
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
+            if(c != null){
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                }
+            }
+        }
     }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao userDao = new UserDao(new AWSConnectionMaker());
+//        UserDao userDao = new UserDao(new AWSConnectionMaker());
 //        userDao.add();
-        User user = userDao.getById("1");
-        System.out.println(user.getName());
+//        User user = userDao.getById("1");
+//        System.out.println(user.getName());
 
     }
 }
--- src/test/java/dao/UserDaoTest.java
+++ src/test/java/dao/UserDaoTest.java
@@ -1,16 +1,20 @@
 package dao;
 
 import domain.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = UserDaoFactory.class)
@@ -18,37 +22,35 @@ class UserDaoTest {
 
     @Autowired
     ApplicationContext context;
+    UserDao userDao;
+    User user1 = new User("1","AAA","1111");
+    User user2 = new User("2","BBB","2222");
+    User user3 = new User("3","CCC","3333");
+
+    @BeforeEach
+    void setUP(){
+        this.userDao = context.getBean("awsUserDao", UserDao.class);
+    }
 
     @Test
     void addAndGet() throws SQLException, ClassNotFoundException {
-        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
-        userDao.deleteAll();
-        assertEquals(0, userDao.getCount());
-
-        String id = "8";
-
-        userDao.add(new User(id, "HHH", "8888"));
-        User user = userDao.getById(id);
-
-        assertEquals("HHH", user.getName());
-
-    }
-    @Test
-    void deleteAndCount() throws SQLException, ClassNotFoundException {
-        UserDao userDao = context.getBean("awsUserDao",UserDao.class);
-        User user1 = new User("1","AAA","1111");
-        User user2 = new User("2","BBB","2222");
-        User user3 = new User("3","CCC","3333");
-
         userDao.deleteAll();
         assertEquals(0, userDao.getCount());
 
         userDao.add(user1);
         assertEquals(1, userDao.getCount());
-        userDao.add(user2);
-        assertEquals(2, userDao.getCount());
-        userDao.add(user3);
-        assertEquals(3, userDao.getCount());
+
+        User user = userDao.getById(user1.getId());
+        assertEquals(user1.getName(), user.getName());
+        assertEquals(user1.getPassword(), user.getPassword());
     }
 
+    @Test
+    @DisplayName("User == null인 경우 Exception")
+    void userNull() {
+        assertThrows(EmptyResultDataAccessException.class, ()->{
+            userDao.deleteAll();
+            userDao.getById("0");
+        });
+    }
 }
